6ac1a4f09e2de6140deb945b9929bb7f21241c6a
jeremycann <jeremycann@gmail.com>
2015-10-01 09:40:12 -0400

    Create Runtime.js
    Runtime file used for global variables.

---
+++ Runtime.js
@@ -0,0 +1,7 @@
+Ext.define('Runtime',{
+	    singleton : true,
+	    globalContent_id : '2',   // initialize to emptyString
+	    currentContentID: ''
+	    //constructor : function(config){
+	    //    this.initConfig(config);
+	});
